909e5bca17b4e06b4df4980e569185686bd168be
JuanCarlos090897 <36517516+JuanCarlos090897@users.noreply.github.com>
2020-01-21 12:29:16 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SIM_Lubricentro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
