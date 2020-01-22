32fa07b322e9c5e96ece279e4e96de3c01228187
JuanCarlos090897 <36517516+JuanCarlos090897@users.noreply.github.com>
2020-01-21 21:54:53 -0600

    parte 2

---
+++ Models/Carro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIM_Lubricentro.Models
+{
+    public class Carro
+    {
+        public int ID { get; set; }
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+    }
+}
